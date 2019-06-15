28f1e76f8f1d13d7414da8c836da8d74d537780b
SergeyEleev <51604015+SergeyEleev@users.noreply.github.com>
2019-06-15 22:36:17 +0500

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,8 +1,4 @@
  <?php
-        $hash = $_GET['hash'];
-
-        if($hash == md5('614121390759251MXvRdponYG8lktKIhh9I')) { ?>
+       
             <h1>Hello</h1>
-        <?php } else { ?>
-            <h1>Ошибка входа</h1>
-        <?php } ?>
+    
